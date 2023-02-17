2b16d7e3dfeec6c10a90615b2a83c012b8bd3da6
shourovahmd <moh.abuhanifa@gmail.com>
2023-02-17 19:14:33 +0600

    userModel

--- server.js
+++ server.js
@@ -5,11 +5,31 @@ const cors = require("cors");
 require("dotenv").config();
 app.use(express.json());
 app.use(cors());
+const mongoose = require("mongoose");
+const User = require("./model/userModel");
+mongoose.set("strictQuery", false);
 
-app.post("/api/register", (req, res) => {
-  const { name } = req.body;
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.4fc0zmu.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => console.log(`MongoDb server connected`))
+  .catch((err) => console.log(err));
+
+app.post("/api/register", async (req, res) => {
   console.log(req.body);
-  res.send({ name });
+
+  try {
+    const { name, email, password } = req.body;
+    const user = await User.create({
+      name: name,
+      email: email,
+      password: password,
+    });
+    res.send({ name });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 app.listen(port, () => {
